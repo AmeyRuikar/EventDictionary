99568e6c4de48460158820f0a323a8227cc530c7
Amey Ruikar <ruikar@usc.edu>
2016-10-13 23:28:28 -0700

    yelp full extractor

--- yelp_extractor.py
+++ yelp_extractor.py
@@ -14,7 +14,7 @@ client = Client(auth)
 response = client.search('Downtown Los Angeles')
 
 businesses = response.businesses
-print "results:" +str(len(businesses))+'\n'
+#print "results:" +str(len(businesses))+'\n'
 
 for business in businesses:
     #print business.name
@@ -24,4 +24,15 @@ for business in businesses:
     rating = business.rating
     urlBooking = business.reservation_url
     url = business.url
-    print name +"->"+str(rating)+":"+url+"\n"
+    lat = business.location.coordinate.latitude
+    longitude = business.location.coordinate.longitude
+    dealStart = 0
+    dealEnd = 0
+
+    if business.deals:
+        print business.deals
+        dealStart = business.deals.time_start
+        dealEnd = business.deals.time_end
+
+    #print name +"->"+str(rating)+":"+str(lat)+", "+str(longitude)+" deal from "+str(dealStart)+"\n"
+    print '''INSERT INTO events("eventID", "eventName", "startTime", "endTime", "urlEvent", "urlBooking", "urlImg", latitude , longitude, "genre", "subgenre", rating, "saleStartTime", "saleEndTime")  VALUES ('%s', '%s', '2016-09-29T14:00:00Z', '2016-09-29T14:00:00Z', '%s', '%s', '%s', %f, %f, '%s', '%s', %f, '2016-09-29T14:00:00Z', '2016-09-29T14:00:00Z');'''%(eventId, name, url, urlBooking, urlImg, lat, longitude, 'food', '', rating)  
